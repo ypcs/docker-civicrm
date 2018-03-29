f1ace69a9c12a4d9a113e6fe0fbf72196613dafd
Ville Korhonen <ville@xd.fi>
2018-03-29 12:32:07 +0300

    Add working install script

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ TARGET="$1"
 
 [ -z "${CIVICRM_DATABASE_DSN}" ] && echo "missing variable: \$CIVICRM_DATABASE_DSN" && exit 1
 
+cd "${TARGET}"
 cv core:install \
-    --db="${CIVICRM_DATABASE_DSN}"
+    --db="${CIVICRM_DATABASE_DSN}" \
+    --cms-base-url="${CIVICRM_URL}"
